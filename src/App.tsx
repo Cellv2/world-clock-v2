35adf49d76b27a70c4dcaaab24b54157ee45e151
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-10-06 20:44:38 +0100

    :construction: Implement redux slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+import { WorldTimeApiService } from "./services/world-time-api/world-time-api.service";
+import { useDispatch } from "react-redux";
+import { fetchAllTimezones, getTimezonesAsync } from "./app/time.slice";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    // const svc = new WorldTimeApiService();
+    // svc.getAllTimezones().then(data => console.log(data))
+
+    const dispatch = useDispatch();
+
+    // dispatch(getTimezonesAsync());
+    dispatch(fetchAllTimezones())
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Counter />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <span>
+                    <span>Learn </span>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React
+                    </a>
+                    <span>, </span>
+                    <a
+                        className="App-link"
+                        href="https://redux.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Redux
+                    </a>
+                    <span>, </span>
+                    <a
+                        className="App-link"
+                        href="https://redux-toolkit.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Redux Toolkit
+                    </a>
+                    ,<span> and </span>
+                    <a
+                        className="App-link"
+                        href="https://react-redux.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React Redux
+                    </a>
+                </span>
+            </header>
+        </div>
+    );
 }
 
 export default App;
