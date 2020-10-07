be1e1c70d1b5a82a17264594a35e9fcbcf039e66
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-10-07 23:49:43 +0100

    Add a few test descriptions

---
+++ src/services/world-time-api/world-time-api.service.test.ts
@@ -0,0 +1,38 @@
+import { WorldTimeApiService } from "./world-time-api.service";
+
+describe("services - world-time-api", () => {
+    // it("should return a string[] of timezones", () => {
+    //     const exampleTimezones = [
+    //         "Africa/Abidjan",
+    //         "Africa/Accra",
+    //         "Africa/Algiers",
+    //         "Africa/Bissau",
+    //         "Africa/Cairo",
+    //         "Africa/Casablanca",
+    //         "Africa/Ceuta",
+    //         "Africa/El_Aaiun",
+    //         "Africa/Johannesburg",
+    //         "Africa/Juba",
+    //         "Africa/Khartoum",
+    //         "Africa/Lagos",
+    //         "Africa/Maputo",
+    //         "Africa/Monrovia",
+    //         "Africa/Nairobi",
+    //         "Africa/Ndjamena",
+    //         "Africa/Sao_Tome",
+    //         "Africa/Tripoli",
+    //         "Africa/Tunis",
+    //         "Africa/Windhoek",
+    //         "America/Adak",
+    //         "America/Anchorage",
+    //         "America/Araguaina",
+    //         "America/Argentina/Buenos_Aires",
+    //         "America/Argentina/Catamarca",
+    //         "America/Argentina/Cordoba",
+    //         "America/Argentina/Jujuy",
+    //         "America/Argentina/La_Rioja",
+    //     ];
+    // });
+
+    it("should set _useIp to true on construction");
+});
