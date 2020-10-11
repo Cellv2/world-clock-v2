ebe50f03876c32a4f5e4262268db24093a084565
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-10-11 23:44:52 +0100

    Add reject test

--- src/services/world-time-api/world-time-api.service.test.ts
+++ src/services/world-time-api/world-time-api.service.test.ts
@@ -33,17 +33,14 @@ jest.mock("./world-time-api.service", () => {
 describe("services - world-time-api", () => {
     const service = new Service();
     const mockedService = mocked(service, true);
+    const spyGetAllTimezones = jest.spyOn(mockedService, "getAllTimezones");
+
+    beforeEach(() => {
+        // avoid shared spy calls from bleeding across tests
+        jest.clearAllMocks();
+    });
 
     describe("should set timezones array when resolving", () => {
-        beforeEach(() => {
-            // avoid shared spy calls from bleeding across tests
-            jest.clearAllMocks();
-            // jest.resetAllMocks();
-            // jest.restoreAllMocks();
-        });
-
-        const spyGetAllTimezones = jest.spyOn(mockedService, "getAllTimezones");
-
         it("should resolve", () => {
             expect(mockedService.getAllTimezones()).resolves.not.toThrow();
         });
@@ -53,10 +50,21 @@ describe("services - world-time-api", () => {
             expect(spyGetAllTimezones).toHaveBeenCalled();
             expect(spyGetAllTimezones).toHaveBeenCalledTimes(1);
 
-            // expect(mockedService.getAllTimezones.mock.results).resolves.not.toThrow()
-            // console.dir(mockedService.getAllTimezones.mock.results)
             expect(mockedService.timezones).toEqual(apiTimezones);
         });
+    });
+
+    it("should throw an error when rejecting", async () => {
+        mockedService.getAllTimezones.mockImplementationOnce(async () => {
+            return Promise.reject(new Error("Failed to get timezones"));
+        });
+
+        expect(mockedService.getAllTimezones()).rejects.toThrow(
+            new Error("Failed to get timezones")
+        );
+
+        expect(spyGetAllTimezones).toHaveBeenCalled();
+        expect(spyGetAllTimezones).toHaveBeenCalledTimes(1);
     });
 
     xit("SHOULD BE TESTED!", () => {});
