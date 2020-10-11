cb0c34e538343751f78fe4c78d6ddfe0f0d5e842
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-10-11 23:29:52 +0100

    Add resolve tests

--- src/services/world-time-api/world-time-api.service.test.ts
+++ src/services/world-time-api/world-time-api.service.test.ts
@@ -1,20 +1,62 @@
 import { mocked } from "ts-jest/utils";
 
-import Service from "./world-time-api.service";
 import apiTimezones from "./__tests__/apiData.json";
+
+import Service from "./world-time-api.service";
+
+// mocked to avoid API calls
+// https://jestjs.io/docs/en/tutorial-async
+jest.mock("./world-time-api.service", () => {
+    const apiTimezones = require("./__tests__/apiData.json");
+    return class {
+        timezones = [];
+        // shouldResolve is only part of the test mock - it does not exist in the normal implementation
+        async getAllTimezones(shouldResolve: boolean = true): Promise<void> {
+            return new Promise((resolve, reject) => {
+                process.nextTick(() => {
+                    if (shouldResolve) {
+                        this.timezones = apiTimezones;
+                        return resolve();
+                    } else {
+                        // reject({Error: "Failed to get timezones"})
+                        return reject("Failed to get timezones");
+                    }
+
+                    // reject({Error: "Failed to get timezones"});
+                    // return;
+                });
+            });
+        }
+    };
+});
 
 describe("services - world-time-api", () => {
     const service = new Service();
     const mockedService = mocked(service, true);
 
-    it("has more tests, precious", async () => {
+    describe("should set timezones array when resolving", () => {
+        beforeEach(() => {
+            // avoid shared spy calls from bleeding across tests
+            jest.clearAllMocks();
+            // jest.resetAllMocks();
+            // jest.restoreAllMocks();
+        });
+
         const spyGetAllTimezones = jest.spyOn(mockedService, "getAllTimezones");
-        await mockedService.getAllTimezones(); // this is not a good idea (it calls an actual API), but I'm still learning
 
-        expect(spyGetAllTimezones).toHaveBeenCalled();
-        expect(spyGetAllTimezones).toHaveBeenCalledTimes(1);
+        it("should resolve", () => {
+            expect(mockedService.getAllTimezones()).resolves.not.toThrow();
+        });
 
-        expect(mockedService.timezones).toEqual(apiTimezones);
+        it("should set timezones", async () => {
+            await mockedService.getAllTimezones();
+            expect(spyGetAllTimezones).toHaveBeenCalled();
+            expect(spyGetAllTimezones).toHaveBeenCalledTimes(1);
+
+            // expect(mockedService.getAllTimezones.mock.results).resolves.not.toThrow()
+            // console.dir(mockedService.getAllTimezones.mock.results)
+            expect(mockedService.timezones).toEqual(apiTimezones);
+        });
     });
 
     xit("SHOULD BE TESTED!", () => {});
