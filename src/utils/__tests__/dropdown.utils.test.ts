b6c573de2aba68d06b9f24bea8a257230ea07a6a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-12-14 23:33:38 +0000

    :white_check_mark: Add test

---
+++ src/utils/__tests__/dropdown.utils.test.ts
@@ -0,0 +1,42 @@
+import { mocked } from "ts-jest/utils";
+
+import * as dropdownUtils from "../dropdown.utils";
+
+const mockedDropdownUtils = mocked(dropdownUtils, true);
+
+type CustomDropdownValue = { label: string; value: string };
+
+describe("test valueToCustomDropdownValue", () => {
+    // const mockedValueToCustomDropdownValue = mockedDropdownUtils.valueToCustomDropdownValue;
+    const mockedValueToCustomDropdownValue = mocked(
+        dropdownUtils.valueToCustomDropdownValue,
+        true
+    );
+
+    describe.each([
+        ["KEKW", { label: "KEKW", value: "KEKW" }],
+        ["SomeValue", { label: "SomeValue", value: "SomeValue" }],
+        ["El_Risitas", { label: "El Risitas", value: "El_Risitas" }],
+        [
+            "Lots_Of_Underscores",
+            { label: "Lots Of Underscores", value: "Lots_Of_Underscores" },
+        ],
+        [
+            "Double__Underscores",
+            { label: "Double  Underscores", value: "Double__Underscores" },
+        ],
+    ])("%s", (val, expected) => {
+        test(`returns ${expected}`, () => {
+            expect(
+                mockedValueToCustomDropdownValue(val)
+            ).toMatchObject<CustomDropdownValue>(expected);
+        });
+    });
+});
+
+describe("test generateSelectOptions", () => {
+    const mockedGenerateSelectOptions =
+        mockedDropdownUtils.generateSelectOptions;
+
+    xit("Tests to be written", () => {});
+});
