209483781bf79abff5db7bc15e2bb5a1f0339833
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-12-17 23:13:16 +0000

    :white_check_mark: Add test

--- src/utils/__tests__/string.utils.test.ts
+++ src/utils/__tests__/string.utils.test.ts
@@ -2,10 +2,6 @@ import { mocked } from "ts-jest/utils";
 
 import * as stringUtils from "../string.utils";
 
-describe("test replaceSlashAndUnderscore", () => {
-    const fnToTest = mocked(stringUtils.replaceSlashAndUnderscore, true);
-});
-
 describe("test replaceSlashWithCommaWhitespace", () => {
     const fnToTest = mocked(stringUtils.replaceSlashWithCommaWhitespace, true);
     describe.each([
@@ -19,6 +15,7 @@ describe("test replaceSlashWithCommaWhitespace", () => {
     });
 });
 
+// TODO: add test case for _ at the start - what is the expected behaviour here?
 describe("test replaceUnderscoreWithWhitespace", () => {
     const fnToTest = mocked(stringUtils.replaceUnderscoreWithWhitespace, true);
     describe.each([
@@ -26,6 +23,18 @@ describe("test replaceUnderscoreWithWhitespace", () => {
         ["Two__Underscore", "Two Underscore"],
         ["THREE___Underscore, ah_ah_ahh", "THREE Underscore, ah ah ahh"],
         ["'3£'@__AwdasdD_x!!@_", "'3£'@ AwdasdD x!!@ "],
+    ])("%s", (val, expected) => {
+        it(`returns ${expected}`, () => {
+            expect(fnToTest(val)).toBe(expected);
+        });
+    });
+});
+
+describe("test replaceSlashAndUnderscore", () => {
+    const fnToTest = mocked(stringUtils.replaceSlashAndUnderscore, true);
+    describe.each([
+        ["One_Underscore/One Slash", "One Underscore, One Slash"],
+        ["Mix/_/it__//Up", "Mix,  , it , Up"],
     ])("%s", (val, expected) => {
         it(`returns ${expected}`, () => {
             expect(fnToTest(val)).toBe(expected);
