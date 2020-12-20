d86f3a7e09fa32b53c57a8947fbe9f85d6561976
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-12-20 12:30:30 +0000

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
-
-import { WorldTimeApiService } from "./services/world-time-api/world-time-api.service";
-import { useDispatch } from "react-redux";
-import { fetchAllTimezones, getCurrentTimeInTimezone } from "./app/time.slice";
 
 import Clock from "./components/Clock";
 
 import styles from "./App.module.scss";
 
 function App() {
-    // const svc = new WorldTimeApiService();
-    // svc.getAllTimezones().then(data => console.log(data))
-
-    const dispatch = useDispatch();
-
-    // dispatch(getTimezonesAsync());
-    // dispatch(fetchAllTimezones());
-    // dispatch(getCurrentTimeInTimezone());
-
     return (
         <div className={styles.app}>
             <div className={styles.container}>
